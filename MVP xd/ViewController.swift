9003ff41331d1b73d019e5db2cccc25ad547de1e
Vansa Pha <vs.lov.rs@gmail.com>
2017-11-07 13:47:12 +0700

    Initial Commit

---
+++ MVP xd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVP xd
+//
+//  Created by Vansa Pha on 11/7/17.
+//  Copyright © 2017 Vansa Pha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
